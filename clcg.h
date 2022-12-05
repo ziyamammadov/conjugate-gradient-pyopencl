75cba17a454bf7caf8d79686ba2ad5c4a86a757d
Ziya Mammadov <55743413+ziyamammadov@users.noreply.github.com>
2022-12-05 11:09:56 +0200

    Update clcg.h

--- clcg.h
+++ clcg.h
@@ -1,7 +1,7 @@
 #ifndef OCLCG_CLCG_H
 #define OCLCG_CLCG_H
 float* cg(int size, int nonZeros,
-        const float *aValues, const int *aPointers, const int *aCols,
-        const float *b, float *x, int nRHS, int nIterations, int isComplex);
+        const float *aValues, const float *b, const int *aPointers, 
+        const int *aCols, float *x, int nRHS, int nIterations, int isComplex);
 
 #endif //OCLCG_CLCG_H
